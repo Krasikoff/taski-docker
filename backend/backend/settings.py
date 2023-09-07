4c9c8b97a109793e71fe95f226ce06ed8db3d8b2
Красиков Дмитрий <z1r0-d@yandex.ru>
2023-09-07 12:09:42 +0300

    Added load_dotenv.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -60,14 +63,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'debug.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
+	    'default': {
+    	    'ENGINE': 'django.db.backends.postgresql',
+    	    'NAME': os.getenv('POSTGRES_DB', 'django'),
+    	    'USER': os.getenv('POSTGRES_USER', 'django'),
+    	    'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+    	    'HOST': os.getenv('DB_HOST', ''),
+    	    'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
